2c8001e84155ae3904befbc3b1a1083442ed98c6
Kev_2 <riseofrochy@gmail.com>
2024-06-07 18:52:01 -0500

    Resuelve conflictos en Php/conexion.php después del pull

--- Php/conexion.php
+++ Php/conexion.php
@@ -2,7 +2,7 @@
 $host='localhost';
 $root='root';
 $password='';
-$nombrebd='med_test';
+$nombrebd='medtest2';
 $conn= new mysqli($host,$root,$password,$nombrebd);
 if(!$conn){
     die("No es posible conectar a la base de datos");
